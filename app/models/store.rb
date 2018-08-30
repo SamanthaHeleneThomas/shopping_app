e5a3224a5e798504069935effa41533c2f8826cd
Samantha Helene Thomas <2shthomas@gmail.com>
2018-08-29 18:56:52 -0600

    models are made

---
+++ app/models/store.rb
@@ -0,0 +1,17 @@
+class Store < ApplicationRecord
+    #validations
+
+    # assosciations
+    has_many: lists 
+    # put in after you have items and lists
+
+    # callback methods
+
+
+    # instance/ class methods
+    # need them for all the models 
+
+
+
+
+end
